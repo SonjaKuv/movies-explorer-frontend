4055d197388ad1b45577fa0c8d7cf9e2209e7f3d
SonjaKuv <kuvshinova.sonja@yandex.ru>
2023-03-19 18:16:54 +0300

    ready for review

--- src/components/ProfileInfo/ProfileInfo.js
+++ src/components/ProfileInfo/ProfileInfo.js
@@ -5,7 +5,7 @@ function ProfileInfo({title, info}) {
     return (
       <div className='profile-info'>
         <span className='profile-info__title'>{title}</span>
-        <input className='profile-info__input profile-info__data' value={info} readOnly/>
+        <input className='profile-info__input profile-info__data' value={info} readOnly required/>
       </div>
     )
 }
